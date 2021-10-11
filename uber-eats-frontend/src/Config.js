a9b7acbdf2fae6b6e019ea22b0fa1778f9553f36
Rahil Shah <rahilshah@Rahils-MacBook-Air.local>
2021-10-11 00:04:20 -0700

    Mocha Testing

--- uber-eats-frontend/src/Config.js
+++ uber-eats-frontend/src/Config.js
@@ -1,4 +1,4 @@
-// const backendServer = 'http://localhost:8080/uber-eats/api';
-const backendServer = 'http://ec2-3-16-143-37.us-east-2.compute.amazonaws.com:8080/uber-eats/api';
+const backendServer = 'http://localhost:8080/uber-eats/api';
+// const backendServer = 'http://ec2-3-16-143-37.us-east-2.compute.amazonaws.com:8080/uber-eats/api';
 
 export default backendServer;
