3edc6bb7f3ad38e1dfc13ead0adf1ae90af6c169
Rahil Shah <rahilshah@Rahils-MacBook-Air.local>
2021-11-13 19:19:05 -0800

    feat: lab 2 codes

---
+++ LAB2/UberEatsFrontend/src/Config.js
@@ -0,0 +1,3 @@
+const backendServer = 'http://54.174.144.152:3001/uber-eats/api';
+
+export default backendServer;
---
+++ LAB2/new-backend/Config.js
@@ -0,0 +1,12 @@
+const awsAccessKey = "AKIAZB5SOMYZNBCYU5NP";
+
+const awsSecretKey = "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";
+
+const config = {
+  mongoDB:
+    "mongodb+srv://Rahil15:ubereatsrahil@ubereatsfinal.qwep0.mongodb.net/UberEatsFinal?retryWrites=true&w=majority",
+  awsAccessKey,
+  awsSecretKey,
+};
+
+module.exports = config;
